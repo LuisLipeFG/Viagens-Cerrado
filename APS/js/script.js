a1d6bce87595fb10c462a8f49cbcd5c5b4302e4a
LuisLipeFG <165704556+LuisLipeFG@users.noreply.github.com>
2024-05-13 18:52:45 -0300

    Add files via upload

---
+++ APS/js/script.js
@@ -0,0 +1,44 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let imgBtn = document.querySelectorAll('.img-btn');
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+imgBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#imagem-slider').src = src;
+    });
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPreView: 1,
+        },
+        768: {
+            slidesPreView: 2,
+        },
+        1024: {
+            slidesPreView: 3,
+        },
+    },
+});
